45ee6420c701e1853bea9abdb879806f05a16e5d
simone.alcala <simone.alcala@gmail.com>
2022-11-27 01:16:33 -0300

    chore: initial setup and add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  
+  app.enableCors();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true
+    })
+  );
+  
+  const config = new DocumentBuilder()
+    .setTitle('Movie API')
+    .setDescription('Movie API routes')
+    .setVersion('1.0')
+    .addTag('movie')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+      
+  await app.listen(+process.env.PORT || 3000);
 }
 bootstrap();
